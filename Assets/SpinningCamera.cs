4447cea0c8f3791e909eaae765c8f8d95c2db2e9
Ilie Cristian <iliepetrecristi@gmail.com>
2023-05-29 13:02:18 +0300

    Added Main Menu, Skybox and secondary camera

---
+++ Assets/SpinningCamera.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
